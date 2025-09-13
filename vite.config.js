28887b34798dc0279161573ea9ba2b79d38f24fb
rajagokilab <rajagokilab@gmail.com>
2025-09-13 13:08:58 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+// vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig(({ command }) => ({
+//   // base: command === 'serve' ? '/' : '/assets/',  // '/' for dev, '/assets/' for build
+//     base: command === 'serve' ? '/' : '',  // empty string for production
+
+//   plugins: [react(), tailwindcss()],
+//   build: {
+//     outDir: 'dist',
+//     assetsDir: '',
+//   }
+// }))
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/', // no /assets/ prefix
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: '', // put JS/CSS/images at root of dist
+  }
+}))
